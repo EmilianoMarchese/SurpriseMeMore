Fixed error in surprise computation np.exp --> 10**

src/surprisemes/Directed_Graph_Class.py
@@ -309,4 +309,4 @@ class DirectedGraph:
     def _set_solved_problem(self, sol):
         self.solution = sol[0]
         self.log_surprise = sol[1]
-        self.surprise = np.exp(-self.log_surprise)
+        self.surprise = 10**(-self.log_surprise)