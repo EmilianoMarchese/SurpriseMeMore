440105a18d49fcfdec5df8f16f4bd47c755b8393
EmilianoMarchese <emilianomarcheserc@gmail.com>
2021-04-22 16:19:56 +0200

    np.arange changed to range to avoid allocation memory problem/bug.

--- src/surprisemes/cp_functions.py
+++ src/surprisemes/cp_functions.py
@@ -256,27 +256,32 @@ def calculate_surprise_logsum_cp_enhanced(adjacency_matrix,
     if is_directed:
         n_o = core_nodes.shape[0]
         n_p = periphery_nodes.shape[0]
-        V_o = n_o * (n_o - 1)
-        V_c = n_o * n_p * 2
+        V_o = int(n_o * (n_o - 1))
+        V_c = int(n_o * n_p * 2)
 
         l_o, w_o = compute_sum_enh(adjacency_matrix, core_nodes, core_nodes)
-        l_c, w_c = compute_sum_enh(adjacency_matrix,
-                                   core_nodes,
-                                   periphery_nodes) + compute_sum_enh(
-            adjacency_matrix,
-            periphery_nodes,
-            core_nodes)
-        L = np.sum(adjacency_matrix.astype(bool))
-        W = np.sum(adjacency_matrix)
+        l_c1, w_c1 = compute_sum_enh(adjacency_matrix,
+                                     core_nodes,
+                                     periphery_nodes)
+        l_c2, w_c2 = compute_sum_enh(adjacency_matrix,
+                                     periphery_nodes,
+                                     core_nodes)
+
+        l_o = int(l_o)
+        w_o = int(w_o)
+        l_c = int(l_c1 + l_c2)
+        w_c = int(w_c1 + w_c2)
+        L = int(np.sum(adjacency_matrix.astype(bool)))
+        W = int(np.sum(adjacency_matrix))
         #w_p = W - w_o - w_c
         n = n_o + n_p
-        V = n * (n - 1)
+        V = int(n * (n - 1))
 
     else:
         n_o = core_nodes.shape[0]
         n_p = periphery_nodes.shape[0]
-        V_o = n_o * (n_o - 1) / 2
-        V_c = n_o * n_p
+        V_o = int(n_o * (n_o - 1) / 2)
+        V_c = int(n_o * n_p)
 
         l_o, w_o = (compute_sum_enh(adjacency_matrix, core_nodes, core_nodes))
         l_c1, w_c1 = compute_sum_enh(adjacency_matrix,
@@ -286,16 +291,16 @@ def calculate_surprise_logsum_cp_enhanced(adjacency_matrix,
                                      periphery_nodes,
                                      core_nodes)
 
-        l_o = l_o / 2
-        w_o = w_o / 2
-        l_c = (l_c1 + l_c2) / 2
-        w_c = (w_c1 + w_c2) / 2
+        l_o = int(l_o / 2)
+        w_o = int(w_o / 2)
+        l_c = int((l_c1 + l_c2) / 2)
+        w_c = int((w_c1 + w_c2) / 2)
 
-        L = np.sum(adjacency_matrix.astype(bool)) / 2
-        W = np.sum(adjacency_matrix) / 2
+        L = int(np.sum(adjacency_matrix.astype(bool)) / 2)
+        W = int(np.sum(adjacency_matrix) / 2)
         #w_p = (W - w_o - w_c) / 2
         n = n_o + n_p
-        V = n * (n - 1) / 2
+        V = int(n * (n - 1) / 2)
 
     # print("V_o", V_o, "l_o", l_o, "V_c", V_c, "l_c", l_c,
     #      "w_o", w_o, "w_c", w_c, "V", V, "L", L, "W", W)
@@ -318,10 +323,10 @@ def surprise_bipartite_logsum_CP_enh(V_o, l_o, V_c, l_c, w_o, w_c, V, L, W):
     logP2 = logP
     logP3 = logP
 
-    for l_o_loop in np.arange(l_o, min_l_o + 1):
-        for l_c_loop in np.arange(l_c, min_l_o + 1 - l_o_loop):
-            for w_o_loop in np.arange(w_o, W + 1):
-                for w_c_loop in np.arange(w_c, W + 1 - w_o_loop):
+    for l_o_loop in range(l_o, min_l_o + 1):
+        for l_c_loop in range(l_c, min_l_o + 1 - l_o_loop):
+            for w_o_loop in range(w_o, W + 1):
+                for w_c_loop in range(w_c, W + 1 - w_o_loop):
                     if (l_o_loop == l_o) & (l_c_loop == l_c) & (
                             w_o_loop == w_o) & (w_c_loop == w_c):
                         continue
@@ -426,7 +431,11 @@ def calculate_surprise_logsum_cp_continuous(adjacency_matrix,
         p_p = p - p_c - p_x
 
     surprise = continuous_surprise_cp(w_x, w_c, p, w, p_c, p_x)
-    return -np.log10(surprise)
+
+    if surprise:
+        return -np.log10(surprise)
+    else:
+        return surprise
 
 
 def continuous_surprise_cp(w_c, w_o, V, W, V_o, V_c):
